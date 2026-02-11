2519e83e75e251056f19e13af65644e70cb42782
Louh12 <ljh10@kent.ac.uk>
2026-02-11 15:03:23 +0000

    creates list of names, ids, rank and devisions

---
+++ fleet_manager.py
@@ -0,0 +1,5 @@
+n = ["Kirk", "Epstein", "Diddy", "Wendy Williams", "Shakka Khan"]
+r = ["Captain", "Commander", "Lieutenant", "Ensign", "Admiral"]
+d = ["Command", "Command", "Security", "Operations", "Intelligence"]
+IDs = ["67", "68", "69", "70", "71"]
+
